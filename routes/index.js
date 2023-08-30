30528d5737601b9e0a98117bf460e3efc18c0937
programmer-weeb <forprogramming8@gmail.com>
2023-08-30 14:24:41 +0300

    Create routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.get('/',)
+
+router.get('/item/create', )
+router.get('/item/:id/update', )
+router.get('/item/:id/delete',)
+router.get('/item/:id',)
+
+router.post('/item/create', )
+router.post('/item/:id/update', )
+router.post('/item/:id/delete', )
+
+
+router.get('/category/create', );
+router.get('/category/:id/update', );
+router.get('/category/:id/delete', );
+
+router.post('/category/create', );
+router.post('/category/:id/update', );
+router.post('/category/:id/delete', );
+router.get('/category/:id', );
 
 module.exports = router;
