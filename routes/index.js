fd2ce62ab5f75be19d105d6f739cd72e95b707dc
programmer-weeb <forprogramming8@gmail.com>
2023-08-31 11:03:37 +0300

    Added the rest of controllers

--- routes/index.js
+++ routes/index.js
@@ -12,18 +12,18 @@ router.get('/item/:id/update', itemController.getUpdateItem)
 router.get('/item/:id/delete', itemController.getDeleteItem)
 router.get('/item/:id', itemController.getItem)
 
-router.post('/item/create', )
-router.post('/item/:id/update', )
-router.post('/item/:id/delete', )
+router.post('/item/create', itemController.postCreateItem )
+router.post('/item/:id/update',  itemController.postUpdateItem)
+router.post('/item/:id/delete', itemController.postDeleteItem)
 
 
-router.get('/category/create', );
-router.get('/category/:id/update', );
-router.get('/category/:id/delete', );
+router.get('/category/create', categoryController.getCreateCategory);
+router.get('/category/:id/update', categoryController.getUpdateCategory);
+router.get('/category/:id/delete', categoryController.getDeleteCategory);
 
-router.post('/category/create', );
-router.post('/category/:id/update', );
-router.post('/category/:id/delete', );
-router.get('/category/:id', );
+router.post('/category/create', categoryController.postCreateCategory);
+router.post('/category/:id/update', categoryController.postUpdateCategory);
+router.post('/category/:id/delete', categoryController.postDeleteCategory);
+router.get('/category/:id', categoryController.getCategory);
 
 module.exports = router;
