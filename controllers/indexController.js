7cdbc4184e5e7de02c879e7a2b771967f23fc322
programmer-weeb <forprogramming8@gmail.com>
2023-08-31 12:29:07 +0300

    Add pug layout and index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,21 @@
 const Item = require('../models/item')
 const Category = require('../models/category')
 
-const { body, validationResult } = require("express-validator");
+const {body, validationResult} = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
-module.exports = function (req, res, next) {
-    res.send('GET home page')
-}
+module.exports = asyncHandler(async (res, req, next) => {
+    const [items, categories] = await Promise.all([
+        Item.find().exec(),
+        Category.find().exec()
+    ])
+
+    console.log(items)
+    console.log(categories)
+
+    res.render('index', {
+        title: 'slkdfjkdlsfjklsdfjfsd',
+        categories,
+        items,
+    })
+})
