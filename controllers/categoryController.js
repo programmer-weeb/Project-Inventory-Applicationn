59ded4048b6b81323e4f88c8764ae93986bfaed1
programmer-weeb <forprogramming8@gmail.com>
2023-09-01 17:42:29 +0300

    finish postCreateCategory()

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,7 @@
 const Item = require('../models/item')
 const Category = require('../models/category')
 
-const { body, validationResult } = require("express-validator");
+const {body, validationResult} = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 // /category/:id
@@ -26,9 +26,47 @@ exports.getDeleteCategory = function (req, res, next) {
 };
 
 // /category/create
-exports.postCreateCategory = function (req, res, next) {
-    res.send(`POST Create Category`);
-};
+exports.postCreateCategory = [
+    body('name', 'Invalid name')
+        .trim()
+        .isLength({min: 3})
+        .escape(),
+    body('description', 'Invalid description')
+        .trim()
+        .isLength({min: 3})
+        .escape(),
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req)
+
+        const newCategory = new Category({
+            name: req.body.name,
+            description: req.body.description,
+        })
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized values/error messages.
+            // res.render("genre_form", {
+            //     title: "Create Genre",
+            //     genre: genre,
+            //     errors: errors.array(),
+            // });
+            res.send(errors.array())
+            return;
+        }
+
+        // Check if a Category  with same name already exists.
+        const categoryExists = await Category.findOne({name: req.body.name}).exec()
+        if (categoryExists) {
+            res.redirect(categoryExists.url)
+        }
+        else {
+            await newCategory.save()
+            res.redirect(newCategory.url)
+        }
+
+    })
+]
 
 // /category/:id/update
 exports.postUpdateCategory = function (req, res, next) {
