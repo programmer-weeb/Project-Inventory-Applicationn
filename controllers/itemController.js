84e1d971d90ee66998ff6f4e271724190df546b2
programmer-weeb <forprogramming8@gmail.com>
2023-09-02 08:42:44 +0300

    finish postCreateItem (view&&controller)
    there is an error with the view of new added items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -40,9 +40,35 @@ exports.getDeleteItem = function (req, res, next) {
 }
 
 // /item/create
-exports.postCreateItem = function (req, res, next) {
-    res.send(`POST Create Item`);
-}
+exports.postCreateItem = [
+    body('name', 'invalidName')
+        .trim()
+        .isLength({min: 3})
+        .escape(),
+    body('description', 'Invalid description')
+        .trim()
+        .isLength({min: 3})
+        .escape(),
+    body('price', 'Invalid price')
+        .isLength({min: 1})
+        .escape(),
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req)
+        const newItem = new Item({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price,
+        })
+        if (!errors.isEmpty()) {
+            res.send(errors.array())
+        } else {
+            await newItem.save()
+            res.redirect(newItem.url)
+        }
+    })
+]
 
 // /item/:id/update
 exports.postUpdateItem = function (req, res, next) {
